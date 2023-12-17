17210479d4ae565c175c6949b811bf24904097e9
mikualvarado <palvarado0918@gmail.com>
2023-12-17 11:25:53 -0500

    Added the steps

---
+++ index.js
@@ -0,0 +1,22 @@
+/**Desarrolla un programa que calcule el promedio de tres notas ingresadas
+por el usuario, solicitando también el nombre del alumno y la materia.
+Además, el programa debe verificar si las notas ingresadas se encuentran
+dentro del rango válido (por ejemplo, de 0 a 10). Una vez calculado el
+promedio de las tres notas, el programa debe evaluar si dicho promedio es
+mayor o igual a 7.
+Si el promedio es mayor o igual a 7, el programa imprimirá: 'Nombre del
+alumno, ¡felicidades! Has aprobado con un promedio de [indicar
+promedio].' En caso de que el promedio sea menor a 7, el programa
+imprimirá: 'Nombre del alumno, gracias por tu esfuerzo. Nos vemos pronto
+en clase. El promedio obtenido es [indicar promedio].' */
+
+// Ingresar el nombre
+// Ingresar la materia
+// Ingresar notas
+// Agregar controles para que las notas se encuentren entre 0 y 10
+// Calcular promedio
+// Evaluar si el promodio es mayor o igual a 7
+// Si promedio >= 7 
+//       Imprimir 'Nombre del alumno, ¡felicidades! Has aprobado con un promedio de [indicar promedio].'
+// Caso contrario 
+//       'Nombre del alumno, gracias por tu esfuerzo. Nos vemos pronto en clase. El promedio obtenido es [indicar promedio].'
